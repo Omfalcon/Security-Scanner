fca4d045886c551aaf860d3964e4ca311ab9ac21
Om agarwal <black.falcon.x.69@gmail.com>
2025-06-25 13:39:09 +0530

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/global.css">
+    <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.3/html2pdf.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap">
+    <title>S-square Security</title>
+</head>
+<body>
+    <header>
+        <div class="logo">$-$quare $ecurity</div>
+        <div class="navlinks">
+            <a href="index.php">Home</a>
+            <a href="tips.html">Tips</a>
+            <a href="about.html">About</a>
+        </div>
+    </header>
